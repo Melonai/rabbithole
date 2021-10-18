efd7e869f23913e73c38cd00644be38c97cd6635
Mel <einebeere@gmail.com>
2021-10-18 21:55:53 +0200

    Add basic expression repl.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,35 @@ mod interpret;
 mod lex;
 mod parse;
 
+use std::io::{self, Write};
+
 use lex::lexer::Lexer;
 
 use interpret::walker::Walker;
 use parse::parser::Parser;
 
+const PROMPT: &'static str = "🐇: ";
+
 fn main() {
-    let source = "1 * 2 + 3 + (-1)";
-
-    let lexer = Lexer::new(source);
-    let mut parser = Parser::new(lexer);
-
-    let node = parser.parse().expect("Failed parsing.");
-
     let walker = Walker::new();
-    let result = walker.walk(&node).expect("Failed interpreting.");
-    println!("{}", result);
+
+    let mut input_buffer;
+
+    loop {
+        print!("{}", PROMPT);
+        io::stdout().flush().expect("Failed flushing.");
+
+        input_buffer = String::new();
+        io::stdin()
+            .read_line(&mut input_buffer)
+            .expect("Failed reading input.");
+
+        let lexer = Lexer::new(input_buffer.trim());
+        let mut parser = Parser::new(lexer);
+
+        let node = parser.parse().expect("Failed parsing.");
+        let result = walker.walk(&node).expect("Failed interpreting.");
+
+        println!("🥕: {}\n", result);
+    }
 }
